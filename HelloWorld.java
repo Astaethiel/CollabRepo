8a84fe9aa6ec97ad4172ed3a6096f27316c1b0e1
THOMAS UTTING <t.utting.20@abdn.ac.uk>
2021-10-15 10:48:53 +0000

    I added a comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, Tom!");
+        // How did we fuck this up so bad lol //
     }
 }
